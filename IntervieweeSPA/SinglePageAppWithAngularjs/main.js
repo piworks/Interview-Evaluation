a02274f4c8b8dc7137ad4557c47a2d010d425180
ibrhmyzc <ibrhmyazici@gmail.com>
2019-02-21 23:00:09 +0300

    generic names changed

--- IntervieweeSPA/SinglePageAppWithAngularjs/main.js
+++ IntervieweeSPA/SinglePageAppWithAngularjs/main.js
@@ -3,6 +3,6 @@ let jquery = require("./Scripts/jquery-1.9.0.js");
 let angular = require("./Scripts/angular.js");
 let ngRoute = require("./Scripts/angular-route.js");
 let bootstrap = require("./Scripts/bootstrap.js");
-let demo = require("./demo.js");
+let appController = require("./app.controller.js");
 let service = require("./service.js");
 let messages = require("./Scripts/angular-messages.js")
